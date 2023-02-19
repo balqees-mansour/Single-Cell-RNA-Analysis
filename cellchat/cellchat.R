77480027c4402344c4e60b45d1f7fb6432ee61ec
balqees mansour <balqeesmansour0@gmail.com>
2023-02-19 17:21:47 +0200

    chatfiles

---
+++ cellchat/cellchat.R
@@ -0,0 +1,95 @@
+library(CellChat)
+library(patchwork)
+options(stringsAsFactors = FALSE)
+# options(
+#   repos = c(
+#     igraph = 'https://igraph.r-universe.dev',
+#     CRAN = 'https://cloud.r-project.org'
+#   )
+#)
+
+# run this command on ternimal
+# conda install gfortran_linux-64
+#install.packages('igraph','1.3.5')
+
+
+# Here we load a scRNA-seq data matrix and its associated cell meta data
+load(url("https://ndownloader.figshare.com/files/25950872")) # This is a combined data from two biological conditions: normal and diseases
+#load("/Users/jinsuoqin/Documents/CellChat/tutorial/data_humanSkin_CellChat.rda")
+data.input = data_humanSkin$data # normalized data matrix
+meta = data_humanSkin$meta # a dataframe with rownames containing cell mata data
+cell.use = rownames(meta)[meta$condition == "LS"] # extract the cell names from disease data
+
+# Prepare input data for CelChat analysis
+data.input = data.input[, cell.use]
+meta = meta[cell.use, ]
+# meta = data.frame(labels = meta$labels[cell.use], row.names = colnames(data.input)) # manually create a dataframe consisting of the cell labels
+unique(meta$labels) # check the cell labels
+
+unique(meta$patient.id)
+
+# Create a CellChat object
+cellchat <- createCellChat(object = data.input, meta = meta, group.by = "labels")
+
+
+cellchat <- addMeta(cellchat, meta = meta)
+cellchat <- setIdent(cellchat, ident.use = "labels") # set "labels" as default cell identity
+levels(cellchat@idents) # show factor levels of the cell labels
+groupSize <- as.numeric(table(cellchat@idents)) # number of cells in each cell group
+
+CellChatDB <- CellChatDB.human # use CellChatDB.mouse if running on mouse data
+showDatabaseCategory(CellChatDB)
+
+# Show the structure of the database
+dplyr::glimpse(CellChatDB$interaction)
+
+# use a subset of CellChatDB for cell-cell communication analysis
+CellChatDB.use <- subsetDB(CellChatDB, search = "Secreted Signaling") # use Secreted Signaling
+# use all CellChatDB for cell-cell communication analysis
+# CellChatDB.use <- CellChatDB # simply use the default CellChatDB
+
+# set the used database in the object
+cellchat@DB <- CellChatDB.use
+
+# subset the expression data of signaling genes for saving computation cost
+cellchat <- subsetData(cellchat) # This step is necessary even if using the whole database
+
+library(doParallel)
+detectCores()
+registerDoParallel(cores = 2)
+
+future::plan("multiprocess", workers = 2) # do parallel
+
+cellchat <- identifyOverExpressedGenes(cellchat)
+cellchat <- identifyOverExpressedInteractions(cellchat)
+
+
+# project gene expression data onto PPI (Optional: when running it, USER should set `raw.use = FALSE` in the function `computeCommunProb()` in order to use the projected data)
+cellchat <- projectData(cellchat, PPI.human)
+
+cellchat <- computeCommunProb(cellchat)
+
+# Filter out the cell-cell communication if there are only few number of cells in certain cell groups
+cellchat <- filterCommunication(cellchat, min.cells = 10)
+
+df.net <- subsetCommunication(cellchat)
+
+cellchat <- computeCommunProbPathway(cellchat)
+
+cellchat <- aggregateNet(cellchat)
+
+cellchat
+
+groupSize <- as.numeric(table(cellchat@idents))
+par(mfrow = c(1,2), xpd=TRUE)
+netVisual_circle(cellchat@net$count, vertex.weight = groupSize, weight.scale = T,  label.edge= T, title.name = "Number of interactions")
+netVisual_circle(cellchat@net$weight, vertex.weight = groupSize, weight.scale = T, label.edge= F, title.name = "Interaction weights/strength")
+
+mat <- cellchat@net$weight
+par(mfrow = c(3,4), xpd=TRUE)
+for (i in 1:nrow(mat)) {
+  mat2 <- matrix(0, nrow = nrow(mat), ncol = ncol(mat), dimnames = dimnames(mat))
+  mat2[i, ] <- mat[i, ]
+  netVisual_circle(mat2, vertex.weight = groupSize, weight.scale = T, edge.weight.max = max(mat), title.name = rownames(mat)[i])
+}
+
